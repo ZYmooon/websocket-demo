40b7fd520d07d366050f7cc23da8fccb6215e701
ZYmooon <1130192385@qq.com>
2018-06-08 04:54:12 +0800

    add README

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var io = require('socket.io')(http)
 
 app.use(express.static('./public'))
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => {   
   socket.on('chat', (user, msg) => {
     //console.log(user + 'say:' + msg + '!')
     io.sockets.emit('chat',user+'：' + msg)
   })
 })
 
-http.listen(3000, function () {
+http.listen(3000, function () {  //监听3000端口：http://127.0.0.1：3000 
   console.log('listening on *:3000')
 })
